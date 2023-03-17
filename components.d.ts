1d5ff5b19e42a6419c6fddb5b8419b2246a8393d
riffca <riffca@riffcas-MacBook-Pro.local>
2023-03-17 23:48:55 +0300

    add left-side

--- components.d.ts
+++ components.d.ts
@@ -15,6 +15,7 @@ declare module '@vue/runtime-core' {
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
     IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
     IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
+    LeftSide: typeof import('./src/components/left-side.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
